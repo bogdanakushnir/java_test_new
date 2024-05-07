c790be09c85de4aa7dda19d2d9148ca9361e4dfa
Bogdana <bogdana.kushnir@sonarsource.com>
2024-05-07 13:33:17 +0200

    test

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("This is project 1");
+    // sjdfghkdjhfgkj
+    String newS ="";
+    int i;
+
+    if(true==true){
+      System.out.println(newS);
+    }
+  }
+}
